7a41f0a9e55f2cec47d7b9829236f29e9abb8a9c
rwoj <rwoj@poczta.onet.pl>
2020-04-05 18:00:02 +0200

    products by identity

--- src/pages/products.js
+++ src/pages/products.js
@@ -1,36 +1,65 @@
 import React from "react"
 import { graphql, Link } from "gatsby"
 import Img from "gatsby-image"
+import netlifyIdentity from "netlify-identity-widget"
 
 import Layout from "../components/layout"
 // import ProductTemplate from "../templates/product-template"
 
-const Products = ({ data: { allContentfulProduct } }) => {
-  return (
-    <Layout>
-      <div>
-        {allContentfulProduct.edges.map(({ node: product }) => (
-          <div key={product.id}>
-            <h2>Garb Products</h2>
-            <Link
-              to={`/products/${product.slug}`}
-              style={{ textDecoration: "none", color: "#551a8b" }}
-            >
-              <h3>
-                {product.name}{" "}
-                <span
-                  style={{ fontSize: "1.2rem", fontWeight: 300, color: "#f60" }}
-                >
-                  ${product.price}
-                </span>
-              </h3>
-            </Link>
-            <Img style={{ maxWidth: "400 px" }} fluid={product.image.fluid} />
-          </div>
-        ))}
-      </div>
-    </Layout>
-  )
+class Products extends React.Component {
+  state = {
+    products: [],
+  }
+
+  componentDidMount() {
+    this.getProducts()
+    netlifyIdentity.on("login", user => this.getProducts(user))
+    netlifyIdentity.on("logout", () => this.getProducts())
+  }
+
+  getProducts = user => {
+    console.log(user)
+    const allProducts = this.props.allContentfulProduct.edges
+
+    const products =
+      netlifyIdentity.currentUser() !== null
+        ? allProducts
+        : allProducts.filter(({ node: product }) => !product.private)
+    this.setState({ products })
+  }
+
+  render() {
+    const { products } = this.state
+    return (
+      <Layout>
+        <div>
+          {products.map(({ node: product }) => (
+            <div key={product.id}>
+              <h2>Garb Products</h2>
+              <Link
+                to={`/products/${product.slug}`}
+                style={{ textDecoration: "none", color: "#551a8b" }}
+              >
+                <h3>
+                  {product.name}{" "}
+                  <span
+                    style={{
+                      fontSize: "1.2rem",
+                      fontWeight: 300,
+                      color: "#f60",
+                    }}
+                  >
+                    ${product.price}
+                  </span>
+                </h3>
+              </Link>
+              <Img style={{ maxWidth: "400 px" }} fluid={product.image.fluid} />
+            </div>
+          ))}
+        </div>
+      </Layout>
+    )
+  }
 }
 
 export const query = graphql`
@@ -42,6 +71,7 @@ export const query = graphql`
           slug
           name
           price
+          private
           image {
             fluid(maxWidth: 400) {
               ...GatsbyContentfulFluid_tracedSVG
